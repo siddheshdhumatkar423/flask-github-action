60ece262221f31e9b2ee1bb1045cca2ca8d7a12f
siddheshdhumatkar423 <siddheshdhumatkar423@gmail.com>
2025-04-16 00:53:42 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,10 +1,9 @@
 terraform {
   backend "s3" {
-    bucket         = "your-terraform-state-bucket123456"
-    key            = "terraform.tfstate"
-    region         = "us-east-1"
-    encrypt        = true
-    
+    bucket         = "your-terraform-state-bucket123456"        # ✅ Replace with your actual S3 bucket name
+    key            = "global/s3/terraform.tfstate"         # Path within the bucket to store the state file
+    region         = "us-east-1"                           # ✅ Your AWS region
+    encrypt        = true                                  # Enable SSE encryption at rest
+            
   }
 }
-
