a15995369cc6cc65af213e4b02c4f5449c264a28
siddheshdhumatkar423 <siddheshdhumatkar423@gmail.com>
2025-04-16 11:16:42 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,8 +1,8 @@
 terraform {
-  backend "s3" {
-    bucket = "my-terraform-state-bucket123456"   # 🔁 Replace with your actual S3 bucket name
-    key    = "env/dev/terraform.tfstate"   # 📁 Path to state file inside the bucket
-    region = "eu-west-2"                   # 🌍 Your AWS region
-    encrypt = true                         # 🔒 Encrypt state file at rest
+  backend "remote" {
+    organization = "TechTrapture"
+    workspaces {
+      name = "test-tfc-demo"
+    }
   }
 }
